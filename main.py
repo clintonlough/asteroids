4cf6f7f67f4ba3957fe10a18bbc3563d0657cbb1
clintonlough <clinton.lough@gmail.com>
2025-08-03 10:14:31 +1000

    Added an asteroid field class and renders moving asteroids

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #Imports
 import pygame
 from constants import *
-from player import *
-from circleshape import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -17,11 +18,15 @@ def main():
     #Setup groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
-    #Instantiate Player Object
+    #Instantiate Objects
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
-
+    asteroid_field = AsteroidField()
 
     #Main Game Loop
     while True:
